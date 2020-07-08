335c0ca26724d9f76513f3e7107d4a232b569444
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2020-07-08 18:29:17 -0300

    Adicionando o controle de rotas

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import * as S from './styles';
+
+const SignIn: React.FC = () => <S.Container />;
+
+export default SignIn;
---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import * as S from './styles';
+
+const SignUp: React.FC = () => <S.Container />;
+
+export default SignUp;
