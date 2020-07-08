51ac8a814962a0d3723a8c8fed3cd5dbe864d6af
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2020-07-08 18:39:38 -0300

    Importando a imagem do logo na tela de login

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Image } from 'react-native';
+
+import logoImg from '../../assets/logo.png';
 
 import * as S from './styles';
 
-const SignIn: React.FC = () => <S.Container />;
+const SignIn: React.FC = () => (
+  <S.Container>
+    <Image source={logoImg} />
+  </S.Container>
+);
 
 export default SignIn;
